e22f4ee1609877b0bfc6a62119589cd272edb5ef
Carolina Lunas <carolinalunasfarah@gmail.com>
2024-05-03 17:07:23 -0400

    test if wepb icons and header image helps optimization

--- src/data/ArchSkills.jsx
+++ src/data/ArchSkills.jsx
@@ -1,27 +1,27 @@
 export const ArchSkills = [
   {
     name: "Autocad",
-    icon_src: "/assets/icons/arch_skills/arch_skills_autocad.png"
+    icon_src: "/assets/icons/arch_skills/arch_skills_autocad.webp"
   },
   {
     name: "Adobe Illustrator",
-    icon_src: "/assets/icons/arch_skills/arch_skills_illustrator.png"
+    icon_src: "/assets/icons/arch_skills/arch_skills_illustrator.webp"
   },
   {
     name: "Adobe Indesign",
-    icon_src: "/assets/icons/arch_skills/arch_skills_indesign.png"
+    icon_src: "/assets/icons/arch_skills/arch_skills_indesign.webp"
   },
   {
     name: "Adobe Photoshop",
-    icon_src: "/assets/icons/arch_skills/arch_skills_photoshop.png"
+    icon_src: "/assets/icons/arch_skills/arch_skills_photoshop.webp"
   },
   {
     name: "Revit",
-    icon_src: "/assets/icons/arch_skills/arch_skills_revit.png"
+    icon_src: "/assets/icons/arch_skills/arch_skills_revit.webp"
   },
   {
     name: "SketchUp",
-    icon_src: "/assets/icons/arch_skills/arch_skills_sketchup.png"
+    icon_src: "/assets/icons/arch_skills/arch_skills_sketchup.webp"
   }
 ]
 
--- src/data/FsSkills.jsx
+++ src/data/FsSkills.jsx
@@ -1,42 +1,42 @@
 export const FsSkills = [
-  {
-    name: "HTML5",
-    icon_src: "/assets/icons/fs_skills/fs_skills_html5.png"
-  },
-  {
-    name: "CSS3",
-    icon_src: "/assets/icons/fs_skills/fs_skills_css3.png"
-  },
-  {
-    name: "Bootstrap",
-    icon_src: "/assets/icons/fs_skills/fs_skills_bootstrap.png"
-  },
-  {
-    name: "Javascript",
-    icon_src: "/assets/icons/fs_skills/fs_skills_javascript.png"
-  },
-  {
-    name: "React JS",
-    icon_src: "/assets/icons/fs_skills/fs_skills_react.png"
-  },
-  {
-    name: "Nest JS",
-    icon_src: "/assets/icons/fs_skills/fs_skills_nest.png"
-  },
-  {
-    name: "PostgreSQL",
-    icon_src: "/assets/icons/fs_skills/fs_skills_postgresql.png"
-  },
-  {
-    name: "Node JS | Express JS",
-    icon_src: "/assets/icons/fs_skills/fs_skills_node.png"
-  },
-  {
-    name: "Git",
-    icon_src: "/assets/icons/fs_skills/fs_skills_git.png"
-  },
-  {
-    name: "GitHub",
-    icon_src: "/assets/icons/fs_skills/fs_skills_github.png"
-  }
-]
+    {
+        name: "HTML5",
+        icon_src: "/assets/icons/fs_skills/fs_skills_html5.webp",
+    },
+    {
+        name: "CSS3",
+        icon_src: "/assets/icons/fs_skills/fs_skills_css3.webp",
+    },
+    {
+        name: "Bootstrap",
+        icon_src: "/assets/icons/fs_skills/fs_skills_bootstrap.webp",
+    },
+    {
+        name: "Javascript",
+        icon_src: "/assets/icons/fs_skills/fs_skills_javascript.webp",
+    },
+    {
+        name: "React JS",
+        icon_src: "/assets/icons/fs_skills/fs_skills_react.webp",
+    },
+    {
+        name: "Nest JS",
+        icon_src: "/assets/icons/fs_skills/fs_skills_nest.webp",
+    },
+    {
+        name: "PostgreSQL",
+        icon_src: "/assets/icons/fs_skills/fs_skills_postgresql.webp",
+    },
+    {
+        name: "Node JS | Express JS",
+        icon_src: "/assets/icons/fs_skills/fs_skills_node.webp",
+    },
+    {
+        name: "Git",
+        icon_src: "/assets/icons/fs_skills/fs_skills_git.webp",
+    },
+    {
+        name: "GitHub",
+        icon_src: "/assets/icons/fs_skills/fs_skills_github.webp",
+    },
+];
